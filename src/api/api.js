88793f9e9b0da6f1f1281fb76285508e37bc058a
Mithin <mithingthomas@gmail.com>
2022-11-28 15:22:14 -0500

    re-add local api

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 let api = process.env.REACT_APP_API_URL;
-// if (process.env.REACT_APP_NODE_ENV !== 'production') {
-//   api = "http://localhost:3080/.netlify/functions/app";
-// }
+if (process.env.REACT_APP_NODE_ENV !== 'production') {
+  api = "http://localhost:3080/.netlify/functions/app";
+}
 
 export default api;
